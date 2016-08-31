54048a6a509d51fa9e0aca307e107690abb68190
Ali96kz <jakparov.kz@gmail.com>
2016-08-31 16:29:36 +0600

    save order in user transaction

--- src/main/java/com/epam/az/flower/shop/service/OrderService.java
+++ src/main/java/com/epam/az/flower/shop/service/OrderService.java
@@ -1,14 +1,21 @@
 package com.epam.az.flower.shop.service;
 
 import com.epam.az.flower.shop.DAO.OrderDAO;
+import com.epam.az.flower.shop.DAO.TransactionDAO;
+import com.epam.az.flower.shop.DAO.UserBalanceDAO;
 import com.epam.az.flower.shop.DAO.UserDAO;
-import com.epam.az.flower.shop.entity.Order;
-import com.epam.az.flower.shop.entity.Product;
-import com.epam.az.flower.shop.entity.User;
+import com.epam.az.flower.shop.entity.*;
+
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class OrderService {
     private OrderDAO orderDAO = new OrderDAO();
     private UserDAO userDAO = new UserDAO();
+    private TransactionDAO transactionDAO = new TransactionDAO();
+    private Transaction transaction = new Transaction();
+    private UserBalanceDAO userBalanceDAO = new UserBalanceDAO();
     public void createOrder(User user, Product product){
         Order order = new Order();
         order.setUser(user);
@@ -17,6 +24,23 @@ public class OrderService {
 
         user.setBalance(user.getBalance()-product.getPrice());
         userDAO.update(user);
+
+        transaction.setId(3);
+        UserBalance userBalance = new UserBalance();
+        userBalance.setTransaction(transaction);
+        userBalance.setUser(user);
+        userBalance.setSum(product.getPrice());
+
+        userBalance.setTransactionDate(getDate());
+
+        userBalanceDAO.insert(userBalance);
+
+    }
+    private java.sql.Date getDate(){
+        Calendar c = new GregorianCalendar();
+        java.util.Date utilDate = c.getTime();
+        java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+        return sqlDate;
     }
 
 }
